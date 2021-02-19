8f4607cc17c05f61c85f79e0de7d3731f2ff8993
skaiser33 <skaiser33@gmail.com>
2021-02-19 15:32:46 -0500

    completes user signup route to handle signup correctly, configures express session middleware, and installs express session, passport, and passport local, and begins configuring passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+  // find or create a user, providing the name and password as default values
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    if (created) {
+      // if created, success and redirect home
+      console.log(`${user.name} was created!`);
+      res.redirect('/');
+    } else {
+      // if not created, the email already exists
+      console.log('Email already exists');
+      res.redirect('/auth/signup');
+    }
+  }).catch(error => {
+    // if an error occurs, let's see what the error is
+    console.log('An error occurred: ', error.message);
+    res.redirect('/auth/signup');
+  });
 });
 
 router.get('/login', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +11,18 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+
+ 
+//* setup the session with the following:
+app.use(session({
+  // * secret: A string used to "sign" the session ID cookie, which makes it unique from application to application. We'll hide this in the environment
+  secret: process.env.SESSION_SECRET,
+  // * resave: Save the session even if it wasn't modified. We'll set this to false
+  resave: false,
+  // * saveUninitialized: If a session is new, but hasn't been changed, save it. We'll set this to true.
+  saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
